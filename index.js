812687c52a1bc68514cbd74c010f4378118e807b
Matt <mdwareing@gmail.com>
2018-08-30 14:12:48 +0100

    Init

---
+++ index.js
@@ -0,0 +1,74 @@
+
+{
+    type: 'REMOVE_TODO',
+    id: 0
+}
+
+
+{
+    type: 'TOGGLE_TODO',
+    id: 0
+}
+
+{
+    type: 'ADD_GOAL',
+    goal: {
+        id: 0,
+        name: 'Run a Marathon'
+    }
+}
+
+{
+    type: 'REMOVE_GOAL',
+    id: 0
+}
+
+function todos(state = [], action) {
+    switch(action.type) {
+        case 'ADD_TODO' :
+            return state.concat([action.todo])
+        case 'REMOVE_TODO' :
+            return state.filter(todo => todo.id !== action.id)
+        case 'TOGGLE_TODO' :
+            return state.map(todo => todo.id !== action.id ? todo : 
+            Object.assign({}, todo, { complete: !todo.complete }))
+
+        default :
+            return state
+    }
+}
+
+function createStore(reducer) {
+    let state
+    let listeners = []
+
+    const getState = () => state
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter(l => l !== listener)
+        }
+    }
+
+    const dispatch = action => {
+        state = reducer(state, action)
+        listeners.forEach(listener => listener())
+    }
+
+    return {
+        getState,
+        subscribe,
+        dispatch
+    }
+}
+
+const store = createStore(todos)
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+})
